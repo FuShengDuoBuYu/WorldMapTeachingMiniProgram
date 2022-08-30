d1faafb0cb34a73aceb2b31ecbef54a8486dc5dd
GuoZhongTian <fushengduobuyu@163.com>
2022-08-31 00:09:32 +0800

    feat : 新增搜索框和顶部跳转按钮

---
+++ pages/index/children/worldMapChart/worldMapChart.vue
@@ -0,0 +1,148 @@
+<template>
+	<view class="content">
+        <echarts ref="echarts" :option="option" canvasId="echarts"></echarts>
+	</view>
+</template>
+
+<script>
+	export default {
+        name: 'worldMapChart',
+		data() {
+			return {
+				option: {},
+				url:''
+			}
+		},
+		onLoad() {
+
+        },
+        onReady() {
+            this.option = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                        type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                    },
+                    confine: true
+                },
+                legend: {
+                    data: ['热度', '正面', '负面']
+                },
+                grid: {
+                    left: 20,
+                    right: 20,
+                    bottom: 15,
+                    top: 40,
+                    containLabel: true
+                },
+                xAxis: [{
+                    type: 'value',
+                    axisLine: {
+                        lineStyle: {
+                            color: '#999'
+                        }
+                    },
+                    axisLabel: {
+                        color: '#666'
+                    }
+                }],
+                yAxis: [{
+                    type: 'category',
+                    axisTick: {
+                        show: false
+                    },
+                    data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+                    axisLine: {
+                        lineStyle: {
+                            color: '#999'
+                        }
+                    },
+                    axisLabel: {
+                        color: '#666'
+                    }
+                }],
+                series: [{
+                    name: '热度',
+                    type: 'bar',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'inside'
+                        }
+                    },
+                    data: [300, 270, 340, 344, 300, 320, 310],
+                    itemStyle: {
+                        // emphasis: {
+                        //   color: '#37a2da'
+                        // }
+                    }
+                },
+                {
+                    name: '正面',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true
+                        }
+                    },
+                    data: [120, 102, 141, 174, 190, 250, 220],
+                    itemStyle: {
+                        // emphasis: {
+                        //   color: '#32c5e9'
+                        // }
+                    }
+                },
+                {
+                    name: '负面',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'left'
+                        }
+                    },
+                    data: [-20, -32, -21, -34, -90, -130, -110],
+                    itemStyle: {
+                        // emphasis: {
+                        //   color: '#67e0e3'
+                        // }
+                    }
+                }
+                ]
+            }
+        },
+        methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,24 +1,39 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<!-- 顶部搜索框和按钮 -->
+		<view style="display: flex;">
+			<uni-combox :candidates="candidates" placeholder="请输入要查找的内容" v-model="searchItem"></uni-combox>
+			<button type="primary" hover-class="button-hover" @click="goto('/pages/chooseCountry/chooseCountry')">国家/国旗</button>
 		</view>
+		<worldMapChartVue></worldMapChartVue>
 	</view>
 </template>
 
 <script>
+	import worldMapChartVue from './children/worldMapChart/worldMapChart.vue';
 	export default {
+		components:{
+            worldMapChartVue
+        },
 		data() {
 			return {
-				title: '测试人2'
+				//用户要搜索的项
+				searchItem:'',
+				//候选城市
+				candidates: ['北京', '上海', '广州', '深圳', '杭州', '成都', '武汉', '南京', '西安', '长沙', '苏州', '天津', '重庆', '郑州', '青岛', '大连', '沈阳', '长春', '哈尔滨', '福州', '厦门', '济南', '石家庄', '太原', '合肥', '南昌', '无锡', '佛山', '东莞', '宁波', '常州', '昆明', '南宁', '贵阳', '海口', '惠州', '珠海', '中山', '烟台', '嘉兴', '南通', '徐州', '温州', '乌鲁木齐', '兰州', '贵阳', '呼和浩特', '保定', '廊坊', '唐山', '邯郸', '秦皇岛', '承德', '沧州', '衡水', '邢台', '张家口', '汕头', '汕尾', '揭阳', '阳江', '茂名', '湛江', '肇庆', '江门', '韶关', '梅州', '河源', '清远', '潮州', '云浮', '台州', '丽水', '金华', '衢州', '舟山', '三亚', '威海', '潍坊', '临沂', '枣庄', '东营', '济宁', '泰安', '莱芜', '滨州', '菏泽', '日照', '咸阳', '宝鸡', '铜川', '渭南', '延安']
+				
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			//跳转页面
+			goto(url) {
+				uni.navigateTo({
+					url:url
+				})
+			}
 		}
 	}
 </script>
