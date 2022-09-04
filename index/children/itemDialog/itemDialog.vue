13a4734961a6e2b14358aa4f1679af6bdcd0ff5a
GuoZhongTian <fushengduobuyu@163.com>
2022-09-05 00:16:32 +0800

    feat : 新增文字转语音,书写itemDialog样式

--- index/children/itemDialog/itemDialog.vue
+++ index/children/itemDialog/itemDialog.vue
@@ -1,70 +1,203 @@
 <template>
-	<view class="contnet">
-		<!-- <image src="../static/map.webp"></image> -->
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
+	<cover-view class="contnet">
+		<cover-image class="image" src="../../../static/map.webp"></cover-image>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn" @click="startToSpeech('字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体')">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
+		<cover-view class="item">
+			<button size="mini" type="primary" class="btn">
+				测试
+			</button>
+			<cover-view class="text">
+				字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体字体
+			</cover-view>
+		</cover-view>
 		
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		<view>123</view>
-		
-	</view>
+	</cover-view>
 </template>
 
 
 <script>
+	var plugin = requirePlugin("WechatSI")
+	let manager = plugin.getRecordRecognitionManager() 
 	export default{
-		name:'itemDialog'
+		name:'itemDialog',
+		data(){
+			return {
+				innerAudioContext : uni.createInnerAudioContext()
+			}
+		},
+		
+		onReady() {
+			
+		},
+		methods:{
+			startToSpeech(textToSpeech){
+				
+				console.log(this.innerAudioContext.paused)
+				if(this.innerAudioContext.paused==false){
+					this.innerAudioContext.pause()
+					uni.showToast({
+						title:"停止播放",
+						duration:1500
+					})
+					
+					return
+				}
+				var that = this
+				plugin.textToSpeech({
+					lang: "zh_CN",
+					    tts: true,
+					    content: textToSpeech,
+					    success: function(res) {
+							that.innerAudioContext.autoplay = true;
+							that.innerAudioContext.src = res.filename;
+							that.innerAudioContext.onPlay(() => {
+							  uni.showToast({
+							  	title:"播放音频",
+								duration:1500
+							  })
+							});   
+					    },
+					    fail: function(res) {
+					        uni.showToast({
+					        	title:"播放失败",
+								icon:"error",
+								duration:1500
+					        })
+					    }
+				})
+			}
+		}
 	}
 </script>
 
 <style>
 	.content{
 		display: flex;
+		/* 内容都居中显示*/
+		justify-content: center;
 		overflow: scroll;
+	}
+	.image{
+		width:100%;
+		margin-top:5%;
+		margin-bottom:5%;
+	}
+	.item{
+		/* 单行展示 */
+		display: flex;
+		/* 内容两侧 */
+		justify-content: space-between;
+		margin: 5%;
+	}
+	.btn{
+		/* 自动换行 */
+		white-space: normal;
+		width: 30vw;
+		padding: 10rpx;
+		margin: 20rpx;
+		border-radius: 20rpx;
+		color: white;
+		/* 更加立体一些 */
+		box-shadow: 0 0 10rpx rgba(0,0,0,0.5);
+	}
+	.text{
+		font-size: 20rpx;
+		/* 自动换行 */
+		white-space: normal;
 	}
 </style>
